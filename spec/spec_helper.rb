af8606f578bf5d0f4198bf4d04bdb1a2176bc1fc
alexander-sysuiev <alexander.sysuiev@gmail.com>
2025-10-26 13:13:19 +0100

    Get rid of rspec status

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,5 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = ".rspec_status"
   config.disable_monkey_patching!
 end
